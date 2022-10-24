8cef2e19144e65f336f7bda50c27c6078e8bd4b4
luistorrest <70786070+luistorrest@users.noreply.github.com>
2022-10-24 17:47:12 -0500

    Código Bluetooth para  comunicación con la App

--- DART/PruebaBT_APP.ino
+++ DART/PruebaBT_APP.ino
@@ -1,22 +1,73 @@
+const int rxPin = 15; //Pin de recepción
+const int txPin = 14; //Pin transmisión
 
+String Dat;
+float Vel,X,Y;
+int i;
+String Shoot;
 
 void setup() {
-
-  //Habilito módulo BT
-  pinMode(pinLed,OUTPUT);
-
-  
-  // Configuración de seriales para comunicación 
-  Serial.begin(9600); // Serial para comunicación con BT
-
+ 
+   Serial.begin(9600); // Monitor serial para corroborar botón presionado
 }
 
 void loop() {
-
-  if(Serial.available()){
-    Serial.println(Serial.readString());
+  
+  //While para recibir parametros iniciales de la app
+  while(1){
+    if(Serial.available()>0){ 
+      Dat = Serial.readString();
+      //Serial.println(Dat);
+  
+      // Se envia pos de X y Y inicial
+      if(Dat.startsWith("P")){
+         i=Dat.indexOf(",");
+         X = Dat.substring(1,i).toFloat();
+         Y = Dat.substring(i+1).toFloat();
+         Serial.println(X);
+         Serial.println(Y);
+      }
+  
+      // Se envía la velocidad del meteorito
+      if(Dat.startsWith("V")){
+        Vel = Dat.substring(1).toFloat();
+        Serial.println(Vel);
+      }
+  
+      // Se mueve hacia la derecha
+      if(Dat.startsWith("R")){
+        Serial.println("Right");
+        break;
+      }
+      // Se mueve hacia la izquierda
+      if(Dat.startsWith("L")){
+        Serial.println("Left");
+        break;
+      }
+      // Se mueve hacia arriba
+      if(Dat.startsWith("U")){
+        Serial.println("Up");
+        break;
+      }
+      // Se mueve hacia abajo
+      if(Dat.startsWith("D")){
+        Serial.println("Down");
+        break;
+      }
+      
+    }
   }
- 
+
+  //While para iniciar movimiento
+  while(1){
+    if(Serial.available()>0){ 
+      Shoot = Serial.read();
+      if(Shoot=="49"){
+        Serial.println("Shooter robot!");
+        break;
+      }
+    }
+   }
+}  
   
   
-}
